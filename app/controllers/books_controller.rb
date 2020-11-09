7d7dc68bc1049d0e421285d416a4886e0bd205da
Olivier d'Otreppe <bestycame@gmail.com>
2020-11-09 16:07:42 +0100

    Added delete button on view and method in controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update]
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
   def index
     @books = Book.all
   end
@@ -19,6 +19,11 @@ class BooksController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path
   end
 
   private
