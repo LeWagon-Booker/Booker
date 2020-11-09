b047ebe31be5b918aa2287afcc5ca9e0fe83092d
thibault alexandre <thibault.a@hotmail.com>
2020-11-09 23:43:09 +0100

    finished the reservation part

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
   has_one_attached :cover
   belongs_to :user
+  has_many :reservations, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :books
-  resources :reservations
-
+  resources :books do
+    resources :reservations, only: [ :create]
+  end
+  resources :reservations, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
