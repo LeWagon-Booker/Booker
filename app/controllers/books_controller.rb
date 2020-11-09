041423e67931e5137acf383e02a5bc93fd5ab2bd
Olivier d'Otreppe <bestycame@gmail.com>
2020-11-09 15:28:08 +0100

    added IMDB parser

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,4 +31,16 @@ class BooksController < ApplicationController
   def set_book
     @book = Book.find(params[:id])
   end
+
+  def parse_by_isbn(isbn)
+    result = {}
+    source = "https://www.googleapis.com/books/v1/volumes?q=isbn%3D#{isbn}&key=#{GOOGLEKEY}"
+    json = JSON.parse(open(source).read, symbolize_names: true)
+    result[:title] = json[:items][0][:volumeInfo][:title]
+    result[:author] = json[:items][0][:volumeInfo][:authors][0]
+    result[:publisher] = json[:items][0][:volumeInfo][:publisher]
+    result[:year] = json[:items][0][:volumeInfo][:publishedDate]
+    result[:description] = json[:items][0][:volumeInfo][:description]
+    result
+  end
 end
