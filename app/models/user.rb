64beb3273521f3dadd953704e9419faf5c2bb632
Olivier d'Otreppe <bestycame@gmail.com>
2020-11-10 14:24:07 +0100

    Avatar with default

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :books
   has_many :reservations, dependent: :destroy
+  has_one_attached :avatar, dependent: :destroy
 end
