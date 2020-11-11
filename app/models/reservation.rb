279e3b582ad7ebcb54ab2aa9b08b4ae949395baa
bestycame <bestycame@gmail.com>
2020-11-11 15:04:32 +0100

    Revert "d"

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,4 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :book
-  attr_accessor :confirmed?
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
     resources :reservations, only: [ :create]
     resources :reviews, only:[:create]
   end
-
-  resources :reservations, only: [:index, :update ]
+  resources :reservations, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
