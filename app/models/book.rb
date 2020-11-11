a8c44fc1d02ca263d8730f1a2b62ddcac4ff4eb5
thibault alexandre <thibault.a@hotmail.com>
2020-11-11 19:53:22 +0100

    added categories

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   has_one_attached :cover
   belongs_to :user
+  belongs_to :category
   has_many :reservations, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
