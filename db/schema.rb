1d88c6797536223abd26468c933ef8b1ef0480af
thibault alexandre <thibault.a@hotmail.com>
2020-11-11 15:48:14 +0100

    reservation finish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_124936) do
+ActiveRecord::Schema.define(version: 2020_11_11_143417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 2020_11_11_124936) do
     t.bigint "book_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "confirmed?", default: false
+    t.boolean "confirmed", default: false
     t.index ["book_id"], name: "index_reservations_on_book_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
