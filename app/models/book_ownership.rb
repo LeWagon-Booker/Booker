bcd3b418e74581036752d8411f38b637cb3dcdee
Olivier d'Otreppe <bestycame@gmail.com>
2020-11-18 17:06:30 +0100

    Test

--- app/models/book_ownership.rb
+++ app/models/book_ownership.rb
@@ -3,7 +3,6 @@ class BookOwnership < ApplicationRecord
   belongs_to :user
   has_many :reservations
 
-
   def unavailable_dates
     reservations.pluck(:start_date, :end_date).map do |range|
       { from: range[0], to: range[1] }
