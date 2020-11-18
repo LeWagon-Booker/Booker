10d0de471f55a749b5a658b93be113a7b90c82a3
Olivier d'Otreppe <bestycame@gmail.com>
2020-11-18 18:05:52 +0100

    default avatar to users and default pictures to families. Selection of users to add to family limited to non added users yet

--- app/models/family.rb
+++ app/models/family.rb
@@ -2,4 +2,11 @@ class Family < ApplicationRecord
   has_one_attached :picture
   has_many :adhesions, dependent: :destroy
   has_many :users, through: :adhesions
+
+  after_save :upload_default_picture
+
+  def upload_default_picture
+    file = File.open("#{Rails.root}/app/assets/images/defaultfamily.png")
+    picture.attach(io: file, filename: 'picture.jpg', content_type: 'image/jpg') unless picture.attached?
+  end
 end
