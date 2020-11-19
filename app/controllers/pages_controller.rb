1e4520dd6a7ea0ab9f3cebff37011531f18cd889
thibault alexandre <thibault.a@hotmail.com>
2020-11-19 18:49:19 +0100

    added a dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,15 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    @user = current_user
+    @books = BookOwnership.where(user_id: @user.id).map(&:book)
+    @wishlist = Wishlist.where(user_id: @user.id).map(&:book)
+    @reviews = Review.where(user_id: @user.id)
+    sql_query = " \
+      adhesions.user_id = :query \
+    "
+    @families = Adhesion.joins(:family).where(sql_query, query: current_user.id).map(&:family)
+  end
 end
