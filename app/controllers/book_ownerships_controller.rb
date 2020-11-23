3e2c5f7a8f8afa5e3f14ae512670321964c41158
Olivier <dOtreppe>
2020-11-23 11:53:32 +0100

    added option to add an ownsership to an existing book

--- app/controllers/book_ownerships_controller.rb
+++ app/controllers/book_ownerships_controller.rb
@@ -1,3 +1,12 @@
 class BookOwnershipsController < ApplicationController
+  def new
+    BookOwnership.create!(book_id: set_params[:book_id], user: current_user)
+    redirect_to book_path(set_params[:book_id])
+  end
 
+  private
+
+  def set_params
+    params.permit(:book_id)
+  end
 end
