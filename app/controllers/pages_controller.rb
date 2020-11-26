0b10576eb4ce01baec947d34a0d7fa00c504f79c
Olivier d'Otreppe <bestycame@gmail.com>
2020-11-26 23:09:38 +0100

    refactoring

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
-  def home
-  end
+  def home; end
 
   def dashboard
     @user = User.find(params[:user_id])
